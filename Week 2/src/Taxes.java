39e4b7945e58277e5b4f5d88615e316b8b0e4282
Rajeevan <rvijay46@gmail.com>
2019-08-13 17:03:16 +0100

    Taxes started

--- Week 2/src/Taxes.java
+++ Week 2/src/Taxes.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Taxes {
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter Salary:");
+        int salary = read.nextInt();
+        percTaxAmount(salary);
+        taxExactAmount(salary);
+    }
+
+    public static int percTaxAmount(int salary){
+        if (salary >= 0 & salary < 14999){
+            return 0;
+        }else if ((salary >= 15000 & salary < 19999)){
+            return 10;
+        }else if (salary >= 20000 & salary < 29999){
+            return 15;
+        }else if (salary >= 30000 & salary < 44999){
+            return 20;
+        }else{
+            return 25;
+        }
+    }
+
+    public static double taxExactAmount(int salary){
+        int taxAmount = percTaxAmount(salary);
+        return salary * 0.1;
+
+
+    }
 }
 
