01a3bf38f3d997c439f706c4ca1db8620999027d
Rajeevan <rvijay46@gmail.com>
2019-08-13 18:15:08 +0100

    Factorial complete

---
+++ Week 2/src/Factorial.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args){
+        ArrayList<Integer> inputs = new ArrayList<>();
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter numbers and type 0 when done:");
+        int input = 0;
+        while (read.hasNextLine()){
+            input = read.nextInt();
+            if (input == 0){
+                break;
+            }else{
+            inputs.add(input);}
+
+        }
+        for (int i: inputs){
+            reverseFactorial(i);
+        }
+
+
+    }
+
+    public static void reverseFactorial(int x){
+        int factorialCounter = 1;
+        int input = x;
+        while (input != 1){
+            if (input % factorialCounter == 0){
+                input /= factorialCounter;
+                factorialCounter++;
+            }else{
+                System.out.println(x + " NONE");
+                return;
+            }
+        }System.out.println(x + "=" + (factorialCounter - 1) + "!");
+
+    }
+}
